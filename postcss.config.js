ef3a18d6fa34381b4a803fafc0c2bfc797976e29
Pascal Lantam Gbate <cyrillantam@gmail.com>
2026-06-13 19:33:13 +0000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
-module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } } 
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+};
 
-
+export default config;
